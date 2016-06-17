3d6d4723143dd9d132ff9f94b69ff31ee9369685
jimmcgaw <jpmcgaw@gmail.com>
2016-06-17 14:20:12 -0700

    hello simple graphql that returns the meaning of life

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import schema from './data/schema';
+import GraphQLHTTP from 'express-graphql';
 import { MongoClient } from 'mongodb';
 
 let app = express();
 app.use(express.static('public'));
+
+app.use('/graphql', GraphQLHTTP({
+  schema
+}));
 
 app.get('/', (request, response) => {
   response.send('hello express!');
